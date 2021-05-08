34ff35e6b2200ad49badb5e35041a036b77e0afe
Eddie <edjwh.ho@gmail.com>
2021-05-08 23:36:29 +0100

    added fetch

--- src/Parent.jsx
+++ src/Parent.jsx
@@ -1,6 +1,6 @@
 import Child from './Child.jsx';
+import Tables from './Table';
 import React from 'react';
-
 
 function Parent() {
     const [value, setValue] = React.useState("eddie");
@@ -8,21 +8,27 @@ function Parent() {
     function handleChange(newValue) {
       console.log("parent rendering...")
       setValue(newValue);
-
-
     }
 
     // We pass a callback to Child
     return  (
-      <div className="parent">
-      <div className="wrapper">
-        <h1>Helloe there, {value}!</h1>
-      <Child value={value} onChange={handleChange} />
-      </div>
+      <div className="parent" style={{
+            position: 'absolute', left: '50%', top: '50%',
+            transform: 'translate(-50%, -50%)'
+        }}
+        >
+        <div className="wrapper">
+          <h1>Helloe there, {value}!</h1>
+          <hr />
+          <Child value={value} onChange={handleChange} />
+        </div>
+        <hr />
+        <div className="table">
+          <Tables />
+        </div>
+        <hr />
       </div>
       )
     }
-
-
 
 export default Parent;
---
+++ src/Table.js
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+import "./styles.css";
+import axios from 'axios';
+
+function Tables() {
+  const [books, setBooks] = useState(null);
+  const apiURL = "https://www.anapioficeandfire.com/api/books?pageSize=30";
+  
+  const fetchData = async () => {
+      const response = await axios.get(apiURL)
+      setBooks(response.data) 
+      console.log(JSON.stringify(response))
+    }
+
+  return (
+    <div className="Tables">
+      <h1>Game of Thrones Books</h1>
+      <h2>Fetch a list from an API and display it</h2>
+
+      {/* Fetch data from API */}
+      <div>
+        <button className="fetch-button" onClick={fetchData}>
+          Fetch Data</button>
+        <br />
+      </div>
+
+      {/* Display data from API */}
+      <div className="books">
+      {books &&
+        books.map((book, index) => {
+            const cleanedDate = new Date(book.released).toDateString();
+            const authors = book.authors.join(', ');
+              return (
+                <div className="book" key={index}>
+                  <h3>Book {index + 1}</h3>
+                  <h2>{book.name}</h2>
+
+                  <div className="details">
+                  <p>👨: {authors}</p>
+                  <p>📖: {book.numberOfPages} pages</p>
+                  <p>🏘️: {book.country}</p>
+                  <p>⏰: {cleanedDate}</p>
+                </div>
+              </div>
+            );
+          })}
+    </div>
+  </div>
+  );
+}
+export default Tables;
